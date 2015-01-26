10f276828232350215fd66f204a086141e7ab6ba
dfroh40 <dmfrohberg@gmail.com>
2015-01-26 18:17:00 -0500

    Ordered constants

--- src/org/usfirst/frc/team1747/robot/RobotMap.java
+++ src/org/usfirst/frc/team1747/robot/RobotMap.java
@@ -1,14 +1,14 @@
 package org.usfirst.frc.team1747.robot;
 
 public class RobotMap {
+	public static final int RIGHT_MINI_CIM_ID=01;
+	public static final int RIGHT_CIM_ID=02;
+	
 	public static final int LEFT_CIM_ID=11;
 	public static final int LEFT_MINI_CIM_ID=12;
 
-	public static final int RIGHT_CIM_ID=2;
-	public static final int RIGHT_MINI_CIM_ID=1;
-	
-	public static final int CENTER_CIM_ID=22;
 	public static final int CENTER_MINI_CIM_ID=21;
+	public static final int CENTER_CIM_ID=22;
 	
 	public static final int ELELVATOR_CIM_LEFT=31;
 	public static final int ELELVATOR_CIM_MIDDLE=32;
